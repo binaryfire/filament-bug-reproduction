f3dac29b521a4442f2e8f5d25c685ad3ee7be7a1
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2025-06-12 02:36:36 +0000

    Update

--- app/Filament/Resources/Users/Schemas/UserInfolist.php
+++ app/Filament/Resources/Users/Schemas/UserInfolist.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\Users\Schemas;
 
 use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Text;
 use Filament\Schemas\Schema;
 
 class UserInfolist
@@ -11,17 +13,32 @@ class UserInfolist
     {
         return $schema
             ->components([
-                // TextEntry::make('uuid')
-                //     ->label('UUID'),
+                Section::make()
+                    ->heading('Information')
+                    ->description('Additional information.')
+                    ->schema([
+                        TextEntry::make('webhook_url')
+                            ->label('Webhook URL')
+                            ->state('https://tenant1.myapp.test/webhooks/incoming/019761a0-003c-7254-a477-a42ae760badd')
+                            ->copyable()
+                            ->copyMessage('Webhook URL copied'),
+                    ]),
+
                 TextEntry::make('name'),
+
                 TextEntry::make('username'),
+
                 TextEntry::make('dob')
                     ->date(),
+
                 TextEntry::make('email'),
+
                 TextEntry::make('email_verified_at')
                     ->dateTime(),
+
                 TextEntry::make('created_at')
                     ->dateTime(),
+
                 TextEntry::make('updated_at')
                     ->dateTime(),
             ]);
