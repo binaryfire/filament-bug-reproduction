dfc3e4cff19176d3795acba0e7abd1f3e30265e8
Raj Siva-Rajah <5361908+binaryfire@users.noreply.github.com>
2025-07-08 01:01:03 +0000

    update

--- app/Models/Code.php
+++ app/Models/Code.php
@@ -5,10 +5,27 @@ namespace App\Models;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
 
 class Code extends Model
 {
     protected $table = 'code';
+
+    /**
+     * The attributes that aren't mass assignable.
+     */
+    protected $guarded = [];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($code) {
+            if (Auth::check()) {
+                $code->user_id = Auth::id();
+            }
+        });
+    }
 
     public function user(): BelongsTo
     {
